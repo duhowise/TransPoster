5d17b9be338078e2cb65694152d9a2e732466d98
Komla Adzam <adzamkomla.dev@gmail.com>
2023-02-01 19:26:11 +0000

    add users locked after 180 days of no login

---
+++ TransPoster.Mvc/Jobs/LockUsersJob.cs
@@ -0,0 +1,39 @@
+﻿using Quartz;
+using TransPoster.Mvc.Services;
+
+namespace TransPoster.Mvc.Jobs;
+
+public class LockUsersJob : IJob
+{
+    private readonly ILogger<LockUsersJob> _logger;
+    private readonly IAuthService _authService;
+
+    public LockUsersJob(ILogger<LockUsersJob> logger, IAuthService authService)
+    {
+        _logger = logger;
+        _authService = authService;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        try
+        {
+            _logger.LogInformation("LOCK USERS JOB STARTED");
+
+            var users = await _authService.UsersWithLogin180DaysAgoAsync();
+
+            foreach (var user in users)
+            {
+                await _authService.LockUserAsync(user.Id);
+            }
+
+            _logger.LogInformation("LOCK USERS JOB ENDED");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogInformation("LOCK USERS JOB FAILED {Exception}", ex);
+
+            throw;
+        }
+    }
+}
--- TransPoster.Mvc/Services/IAuthService.cs
+++ TransPoster.Mvc/Services/IAuthService.cs
@@ -1,6 +1,10 @@
-﻿namespace TransPoster.Mvc.Services;
+﻿using TransPoster.Data.Models;
+
+namespace TransPoster.Mvc.Services;
 
 public interface IAuthService
 {
     public Task<bool> UnlockUserAsync(string id);
+    public Task<bool> LockUserAsync(string id);
+    public Task<IEnumerable<ApplicationUser>> UsersWithLogin180DaysAgoAsync();
 }
