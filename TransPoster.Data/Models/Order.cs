c1f04d812f471a95b0046a0646083decd207f83b
Komla Adzam <adzamkomla.dev@gmail.com>
2023-02-05 04:44:54 +0000

    fix config issues

--- TransPoster.Data/Models/Order.cs
+++ TransPoster.Data/Models/Order.cs
@@ -12,5 +12,4 @@ public class Order : IIdName
     public bool Delivered { get; set; }
     public DateTime? DeliveredAt { get; set; }
     public DateTime CreatedAt { get; set; }
-    public Customer Customer { get; set; }
 }
